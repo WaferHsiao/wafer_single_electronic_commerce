92f595a3a0a0e5e44c56871e85e97b452afec84a
waferhsiao <1586673525@qq.com>
2024-07-11 22:35:12 +0800

    Initial commit

---
+++ src/main/java/com/wafer/wafer_single_electronic_commerce/WaferSingleElectronicCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.wafer.wafer_single_electronic_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaferSingleElectronicCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaferSingleElectronicCommerceApplication.class, args);
+	}
+
+}
