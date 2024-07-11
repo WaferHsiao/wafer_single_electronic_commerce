982b071bf5a444b2a64addafe0fd142d03bbcc64
waferhsiao <1586673525@qq.com>
2024-07-12 07:03:13 +0800

    Initial commit again

---
+++ src/main/java/com/example/commerce/WaferSingleElectronicCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaferSingleElectronicCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaferSingleElectronicCommerceApplication.class, args);
+	}
+
+}
